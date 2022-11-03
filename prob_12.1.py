30af5efb283b00e2584f621054a80074069af780
Edgar Muradyan <90352083+EdgarMuradyan@users.noreply.github.com>
2022-11-03 15:51:24 +0400

    Add files via upload

---
+++ prob_12.1.py
@@ -0,0 +1,12 @@
+
+def repeat(name_list):
+
+	for i in range (len(name_list)):
+
+		for j in range(i + 1):
+			print(name_list[i])
+
+
+
+names = ["Aram", "Alex", "Ani"]
+repeat(names)
---
+++ prob_12.2.py
@@ -0,0 +1,28 @@
+def search_word(word_list, word):
+    result = 0
+    for i in word_list:
+        if i == word:
+            result += 1
+
+    return result
+
+
+def create_dict(text):
+    key_word = ""
+
+    word_dict = {}
+
+    for i in text.split():
+        key_word = i
+        if key_word in word_dict:
+            word_dict[key_word] += 1
+
+        else:
+            word_dict.update({key_word: 1})
+        key_word = ""
+
+
+    return word_dict
+
+text = "ok good ok bad good good banana well banana "
+print(create_dict(text))
